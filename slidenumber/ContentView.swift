cbf9b33cd040915d5f7c1c538820d5946c78b74c
Nutpupicha <6510615096@student.tu.ac.th>
2025-03-26 13:46:06 +0700

    Initial Commit

---
+++ slidenumber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  slidenumber
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 26/3/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ slidenumber/slidenumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  slidenumberApp.swift
+//  slidenumber
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 26/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct slidenumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
